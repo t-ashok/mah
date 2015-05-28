ee6c0ef0cb2b2d998998da07a5875cf1b2d143b7
Tu Trinh <tutrinh@gmail.com>
2015-05-28 01:12:27 -0500

    Tu: Membership

--- js/core-functions.js
+++ js/core-functions.js
@@ -135,6 +135,29 @@ var onReady = {
 		 //	}
 		 //});
 
+		/***************** Waypoints ******************/
+
+		$('.wp1').waypoint(function() {
+			$('.wp1').addClass('animated fadeInLeft');
+		}, {
+			offset: '75%'
+		});
+		$('.wp2').waypoint(function() {
+			$('.wp2').addClass('animated fadeInDown');
+		}, {
+			offset: '75%'
+		});
+		$('.wp3').waypoint(function() {
+			$('.wp3').addClass('animated bounceInDown');
+		}, {
+			offset: '75%'
+		});
+		$('.wp4').waypoint(function() {
+			$('.wp4').addClass('animated fadeInDown');
+		}, {
+			offset: '75%'
+		});
+
 		nav.init();
 		page.init();
 		reloadFunctions.init();
